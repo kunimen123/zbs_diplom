9facba0cb4d3b20c9ce228f32619ed825170b71e
kunimen123 <kudryavsteva.ss@gmail.com>
2026-05-15 02:19:43 +0700

    edit1

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // Для продакшена (Render)
-  static const String baseUrl = 'https://knowledge-base.onrender.com';
+  static const String baseUrl = 'https://knowledge-base-d4sf.onrender.com';
   
   // Для локальной разработки
   // static const String baseUrl = 'http://localhost:8000';
